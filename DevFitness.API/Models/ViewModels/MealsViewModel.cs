a523de5040ae3f972b6445714b0886f06a7f48c5
Manoel Ribeiro <manoelribeiro20181@gmail.com>
2021-06-06 13:38:54 -0300

    Criando padrão ViewModel

---
+++ DevFitness.API/Models/ViewModels/MealsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFitness.API.Models.ViewModels
+{
+    public class MealsViewModel
+    {
+        public int Id { get; private set; }
+        public string Description { get; private set; }
+        public int Calories { get; private set; }
+        public DateTime Date { get; private set; }
+
+        public MealsViewModel(int id, string description, int calories, DateTime date)
+        {
+            Id = id;
+            Description = description;
+            Calories = calories;
+            Date = date;
+        }
+    }
+}
---
+++ DevFitness.API/Models/ViewModels/UserViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFitness.API.Models.ViewModels
+{
+    public class UserViewModel
+    {
+        public int Id { get; private set; }
+        public string FullName { get; private set; }
+        public decimal Height { get; private set; }
+        public decimal Weight { get; private set; }
+        public DateTime BirthDate { get; private set; }
+    }
+}
