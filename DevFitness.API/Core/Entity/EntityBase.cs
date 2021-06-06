0b5f2810084e85fb3cad48cdc5734c7705fb5ac0
Manoel Ribeiro <manoelribeiro20181@gmail.com>
2021-06-06 12:42:53 -0300

    Criando Entity Base

---
+++ DevFitness.API/Core/Entity/EntityBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFitness.API.Core.Entity
+{
+    public class EntityBase
+    {
+        public int Id { get; private set; }
+        public bool Active { get; private set; }
+        public DateTime CreateAt { get; private set; }
+
+        public EntityBase()
+        {
+            Active = true;
+            CreateAt = DateTime.Now;
+        }
+
+
+        protected void Deactivate()
+        {
+            Active = false;
+        }
+    }
+}
