529303915f58779d9b2be03a7cae6e26bacf2ef1
Manoel Ribeiro <manoelribeiro20181@gmail.com>
2021-06-06 14:20:49 -0300

    Criando ~Controllers

---
+++ DevFitness.API/Controllers/MaelsController.cs
@@ -0,0 +1,45 @@
+﻿using DevFitness.API.Models.InputModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFitness.API.Controllers
+{
+    [Route("api/users/{idUSer}/meals")]
+    public class MaelsController: ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll(int idUser)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{idMeals}")]
+        public IActionResult Get(int idUser, int idMeals)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post(int idUser, [FromBody] CreateMealInputModel model)
+        {
+            return Ok();
+        }
+
+
+        [HttpPut("{idMeals}")]
+        public IActionResult Put(int idUSer, int idMeals, [FromBody] UpdateMealInputModel model)
+        {
+            return NoContent();
+        }
+
+        [HttpDelete("{idMeals}")]
+        public IActionResult Delte(int idUser, int idMeals)
+        {
+            return NoContent();
+        }
+    }
+
+}
---
+++ DevFitness.API/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using DevFitness.API.Models.InputModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFitness.API.Controllers
+{
+    [Route("api/users")]
+    public class UserController: ControllerBase
+    {
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateUserInputModel model)
+        {
+            return CreatedAtAction(nameof(Get), new {id = 10 } , model);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id,[FromBody] UpdateUserInputModel model)
+        {
+            return NoContent();
+        }
+    }
+}
